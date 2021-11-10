ec2c2a5957b1db035dc6b686a9911743ce9f94ee
TruongYeuBnLam <93992484+TruongYeuBnLam@users.noreply.github.com>
2021-11-10 09:12:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Hellooo",
-    introTitle: 'Bn iu dấu',
+    introTitle: 'Bn Hân iu dấu',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
